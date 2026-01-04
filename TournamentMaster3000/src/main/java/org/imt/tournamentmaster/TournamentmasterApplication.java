bedd322b104cea5c4320156baec9a34ffc137d6b
Sébastien Palud <sebastien.palud@zenika.com>
2026-01-04 18:41:53 +0100

    4- Add tp4 solution (spring security, tests)

--- TournamentMaster3000/src/main/java/org/imt/tournamentmaster/TournamentmasterApplication.java
+++ TournamentMaster3000/src/main/java/org/imt/tournamentmaster/TournamentmasterApplication.java
@@ -2,8 +2,10 @@ package org.imt.tournamentmaster;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 
 @SpringBootApplication
+@EnableMethodSecurity
 public class TournamentmasterApplication {
 
   public static void main(String[] args) {
