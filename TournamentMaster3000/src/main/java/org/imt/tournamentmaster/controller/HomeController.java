a78f616321e6d5450013840cb19d5cd819170f35
BOURREAUQuentin <bourreauquentin37800@gmail.com>
2026-01-06 15:39:30 +0100

    actuator (todo1 + todo2) + HomeController

---
+++ TournamentMaster3000/src/main/java/org/imt/tournamentmaster/controller/HomeController.java
@@ -0,0 +1,13 @@
+package org.imt.tournamentmaster.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Bienvenue dans Tournament Master!";
+    }
+}
