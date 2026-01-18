ce39d9c987974b594a914cb58aa3313a4157a529
boubast <calbast@gmail.com>
2026-01-18 18:54:39 +0100

    Fix login processing and error access

--- TournamentMaster3000/src/main/java/org/imt/tournamentmaster/configuration/security/SecurityConfiguration.java
+++ TournamentMaster3000/src/main/java/org/imt/tournamentmaster/configuration/security/SecurityConfiguration.java
@@ -1,5 +1,7 @@
 package org.imt.tournamentmaster.configuration.security;
 
+import javax.sql.DataSource;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -10,18 +12,24 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
-import javax.sql.DataSource;
-
 @Configuration
 public class SecurityConfiguration {
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) {
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests(authorize ->
             authorize
+                .requestMatchers("/login", "/perform_login", "/register", "/", "/error", "/403", "/actuator/**").permitAll()
                 .requestMatchers(HttpMethod.GET, "/api/**").hasAnyRole("USER", "ADMIN")
                 .requestMatchers("/api/**").hasRole("ADMIN")
-                .anyRequest().permitAll())
+                .anyRequest().authenticated())
+                .csrf(csrf -> csrf.ignoringRequestMatchers("/api/**")) // CSRF OFF uniquement pour /api/**
+        .exceptionHandling(exception -> exception.accessDeniedPage("/403"))
+        .formLogin(form -> form
+            .loginPage("/login")
+            .loginProcessingUrl("/perform_login")
+            .failureUrl("/login?error")
+            .permitAll())
         .httpBasic(Customizer.withDefaults());
 
         return http.build();
